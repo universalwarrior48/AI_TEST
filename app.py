2b6678c2a2f24fb68a532d02ff9c64075b37d0a1
Universal Warrior <universalwarrior48@gmail.com>
2026-03-22 12:27:02 +0530

    Refactor app.py to move demo launch inside main guard

--- app.py
+++ app.py
@@ -3,5 +3,7 @@ import gradio as gr
 def greet(name):
     return "नमस्ते " + name + "!!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
-demo.launch()
+
+if __name__ == "__main__":
+    demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+    demo.launch()
